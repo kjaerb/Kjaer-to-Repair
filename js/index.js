c890319129554457ca4158e769fa396d59c206e6
bekj <bekj@itu.dk>
2020-10-11 00:20:07 +0200

    Fixing center sections

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
+//Section changing
 new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: true,
-    anchors: ['page1', 'page2', 'page3', 'page4']
+    anchors: ['page1', 'page2', 'page3', 'page4'],
+    loopBottom: true,
+    loopTop: true
 });
