bf11de62d2e0b84d7229467060a0761ff90bbe9a
bekj <bekj@itu.dk>
2020-10-11 19:59:21 +0200

    Centered text in sections on main page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //Section changing
-new fullpage('#fullpage', {
+var fullpage = new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: true,
     anchors: ['page1', 'page2', 'page3', 'page4'],
