6e2629630fb417646fa3852428307fecba91650c
bekj <bekj@itu.dk>
2020-10-11 20:59:13 +0200

    Added gsap

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 //Section changing
-var fullpage = new fullpage('#fullpage', {
+new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: true,
-    anchors: ['page1', 'page2', 'page3', 'page4'],
     loopBottom: true,
-    loopTop: true
+    loopTop: true,
+    onLeave: (origin, destination, direction) => {
+        const section = destination.item;
+        const title = section.querySelector("h1");
+        const tl = new TimelineMax({delay: 0.5});
+        if(title !== null && origin.isLast === false) {
+            tl.fromTo(title, 0.5, { y: "50", opacity: 0}, { y: "0", opacity: 1})
+        }
+    }
+    
 });
