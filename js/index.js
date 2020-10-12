51f6e9f5e4b4c3b98bb5afefa0e8f726686cd5d6
bekj <bekj@itu.dk>
2020-10-12 11:51:47 +0200

    Fixed sizing

--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
+const sections = document.querySelectorAll("section");
+const pointerUp = document.querySelector(".pointer-up");
+
+/*
+//Shows pointer up
+const options = {
+    threshold: 0.7
+};
+
+let observer = new IntersectionObserver(pointerUpCheck, options);
+
+function pointerUpCheck(entries) {
+    entries.forEach(entry => {
+        const tl = new TimelineMax();
+        const className = entry.target.className;
+        console.log(className);
+        if(className !== "section s1 fp-section fp-table active") {
+            tl.fromTo(pointerUp, 0.5, {opacity: 0}, {opacity: 1});
+        } else {
+            tl.fromTo(pointerUp, 0.5, {opacity: 1}, {opacity: 0});
+        }
+    });
+}
+
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+*/
+
 //Section changing
 new fullpage('#fullpage', {
     autoScrolling: true,
@@ -5,6 +35,7 @@ new fullpage('#fullpage', {
     loopBottom: true,
     loopTop: true,
     onLeave: (origin, destination, direction) => {
+        //Animates h1
         const section = destination.item;
         const title = section.querySelector("h1");
         const tl = new TimelineMax({delay: 0.5});
